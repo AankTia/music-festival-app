74fdb6c5f8ff10cb4c4b40f5a7fbbe1f4a2d0a01
TiaWidi <tia.septianawidi@gmail.com>
2025-08-19 22:23:37 +0700

    Create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
@@ -7,3 +9,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+['all', Rails.env].each do |seed|
+  puts "Seed : #{seed}"
+
+  seed_file = Rails.root.join('db', 'seeds', "#{seed}.rb")
+  if File.exist?(seed_file)
+    require seed_file
+  else
+    puts "Seed file is not exists: #{seed_file}"
+  end
+end
