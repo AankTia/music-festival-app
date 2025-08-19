3392383097ae8c5ce0101ba53f41c75cd3180060
TiaWidi <tia.septianawidi@gmail.com>
2025-08-19 11:08:23 +0700

    Configure tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    content: [
+    "./app/views/**/*.(html|turbostream).erb",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.(js|ts|tsx)",
+    "./config/initializers/simple_form_tailwind.rb",
+  ],
+}
